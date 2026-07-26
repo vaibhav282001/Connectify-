582aae5656bc2fbf98312ab3b26f2b09ec9f989d
vaibhav282001 <aiml22_vaibhav.more @isbmcoe.org>
2026-07-26 21:53:58 +0530

    Frontend Deployment on railway

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default: axios} = require('axios');
 
-export const BASE_URL = "https://connectify-shzs.onrender.com/"
+export const BASE_URL = "http://localhost:9080"
 
 export const clientServer = axios.create({
   baseURL: BASE_URL
